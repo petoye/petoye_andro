c17149437c32d967d65dc1cf9b207ae8a54a52ad
Ameya <ameyavi@gmail.com>
2016-09-03 21:47:47 +0530

    adopt image resize done

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -3,7 +3,7 @@ class Adoption < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { original: "414x414>", thumb: "43x43>" }, default_url: "/images/:style/missing.png"
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
   validates_with AttachmentPresenceValidator, attributes: :image
