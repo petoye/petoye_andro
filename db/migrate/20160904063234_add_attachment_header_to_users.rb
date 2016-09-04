f96e5f497b90af86ede67d2a09f372fbeb6c6087
Ameya <ameyavi@gmail.com>
2016-09-04 12:12:37 +0530

    ugh

--- db/migrate/20160904063234_add_attachment_header_to_users.rb
+++ db/migrate/20160904063234_add_attachment_header_to_users.rb
@@ -1,11 +1,11 @@
 class AddAttachmentHeaderToUsers < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
-      t.attachment :header
+      #t.attachment :header
     end
   end
 
   def self.down
-    remove_attachment :users, :header
+    #remove_attachment :users, :header
   end
 end
