47bf2b1120a60881c872402ea35f58799b46880f
Ameya <ameyavi@gmail.com>
2016-10-09 16:27:27 +0530

    pem

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -31,9 +31,9 @@
 
     #push notif
     pusher = Grocer.pusher(
-      certificate: "#{Rails.root}/public/certificate.pem",      # required
+      certificate: "#{Rails.root}/public/certificatedev.pem",      # required
       passphrase:  "1234",                       # optional
-      gateway:     "gateway.push.apple.com", # optional; See note below.
+      gateway:     "gateway.sandbox.push.apple.com", # optional; See note below.
       port:        2195,                     # optional
       retries:     3                         # optional
     ) 
